1b1c5bc66f22ce5c4ef5310f2f6aaec0f5a7fabe
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-20 17:03:38 +0200

    add new views for owner

--- apps/bookings/views/owner_views.py
+++ apps/bookings/views/owner_views.py
@@ -17,10 +17,35 @@ from apps.bookings.serializers import (
 )
 
 
-class BookingOwnerListAPIView(ListAPIView):
+class BookingOwnerListAllAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user)
+
+class BookingOwnerListActiveAPIView(ListAPIView):
+    serializer_class = BookingOwnerListSerializer
+    def get_queryset(self):
+        return Booking.objects.with_details().for_owner(self.request.user).active()
+
+class BookingOwnerListConfirmedAPIView(ListAPIView):
+    serializer_class = BookingOwnerListSerializer
+    def get_queryset(self):
+        return Booking.objects.with_details().for_owner(self.request.user).confirmed()
+
+class BookingOwnerListPendingAPIView(ListAPIView):
+    serializer_class = BookingOwnerListSerializer
+    def get_queryset(self):
+        return Booking.objects.with_details().for_owner(self.request.user).pending()
+
+class BookingOwnerListCancelledOrRejectedAPIView(ListAPIView):
+    serializer_class = BookingOwnerListSerializer
+    def get_queryset(self):
+        return Booking.objects.with_details().for_owner(self.request.user).cancelled_rejected()
+
+class BookingOwnerListCompletedAPIView(ListAPIView):
+    serializer_class = BookingOwnerListSerializer
+    def get_queryset(self):
+        return Booking.objects.with_details().for_owner(self.request.user).completed()
 
 
 class BookingOwnerDetailAPIView(RetrieveAPIView, BookingOwnerLookupMixin):
@@ -43,5 +68,13 @@ class BookingOwnerRejectAPIView(APIView, BookingOwnerLookupMixin):
     def patch(self, request, booking_uuid):
         booking = self.get_booking()
         booking = BookingService.reject(booking)
+        serializer = BookingOwnerDetailSerializer(booking)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BookingOwnerCompleteAPIView(APIView, BookingOwnerLookupMixin):
+    def patch(self, request, booking_uuid):
+        booking = self.get_booking()
+        booking = BookingService.complete(booking)
         serializer = BookingOwnerDetailSerializer(booking)
         return Response(serializer.data, status=status.HTTP_200_OK)
