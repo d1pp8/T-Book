de82fff1070dc1384adeccdeb0ae55729f7f6c5f
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-20 17:02:37 +0200

    add new ends for owners

--- apps/bookings/urls/owner_urls.py
+++ apps/bookings/urls/owner_urls.py
@@ -1,15 +1,33 @@
 from django.urls import path
 
 from apps.bookings.views.owner_views import (
-    BookingOwnerListAPIView,
+    BookingOwnerListActiveAPIView,
+
+    BookingOwnerListAllAPIView,
+    BookingOwnerListConfirmedAPIView,
+    BookingOwnerListPendingAPIView,
+    BookingOwnerListCancelledOrRejectedAPIView,
+    BookingOwnerListCompletedAPIView,
+
     BookingOwnerDetailAPIView,
     BookingOwnerConfirmAPIView,
     BookingOwnerRejectAPIView,
+    BookingOwnerCompleteAPIView
 )
 
 urlpatterns = [
-    path('', BookingOwnerListAPIView.as_view(), name='booking-owner-list-create'),
-    path('<uuid:booking_uuid>/', BookingOwnerDetailAPIView.as_view(), name='booking-owner-list-bookings'),
+
+    path('', BookingOwnerListActiveAPIView.as_view(), name='booking-owner-list-active'),
+
+    path('pending/', BookingOwnerListPendingAPIView.as_view(), name='booking-owner-list-pending'),
+    path('confirmed/', BookingOwnerListConfirmedAPIView.as_view(), name='booking-owner-list-confirmed'),
+    path('cancelled-rejected/', BookingOwnerListCancelledOrRejectedAPIView.as_view(), name='booking-owner-list-cancelled-rejected'),
+    path('completed/', BookingOwnerListCompletedAPIView.as_view(), name='booking-owner-list-completed'),
+    path('all/', BookingOwnerListAllAPIView.as_view(), name='booking-owner-list-all'),
+
+
+    path('<uuid:booking_uuid>/', BookingOwnerDetailAPIView.as_view(), name='booking-owner-detail-bookings'),
     path('<uuid:booking_uuid>/confirm/', BookingOwnerConfirmAPIView.as_view(), name='booking-owner-confim-bookings'),
     path('<uuid:booking_uuid>/reject/', BookingOwnerRejectAPIView.as_view(), name='booking-owner-reject-bookings'),
+    path('<uuid:booking_uuid>/complete/', BookingOwnerCompleteAPIView.as_view(), name='booking-owner-complete-bookings'),
 ]
