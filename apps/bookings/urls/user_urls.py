0df938cf1e66ecc3224309459ad4724d6ed4ba7c
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-20 17:04:50 +0200

    new endspoints for userbooking

--- apps/bookings/urls/user_urls.py
+++ apps/bookings/urls/user_urls.py
@@ -3,13 +3,23 @@ from django.urls import path
 from apps.bookings.views.user_views import (
     BookingUserListCreateAPIView,
     BookingUserUserDetailAPIView,
-    BookingUserUserCancelAPIView
+    BookingUserUserCancelAPIView,
+
+    BookingOwnerListAllAPIView,
+    BookingUserListCompletedAPIView,
+    BookingUserListCancelledOrRejectedAPIView,
+
 )
 
 app_name = 'bookings'
 
 urlpatterns = [
-    path('', BookingUserListCreateAPIView.as_view(), name="booking-user-list-create"),
-    path('<uuid:booking_uuid>/', BookingUserUserDetailAPIView.as_view(), name="booking-user-detail"),
-    path('<uuid:booking_uuid>/cancel/', BookingUserUserCancelAPIView.as_view(), name="booking-user-cancel"),
+    path('', BookingUserListCreateAPIView.as_view(), name='booking-user-list-active-create'),
+
+    path('cancelled-rejected/', BookingUserListCancelledOrRejectedAPIView.as_view(), name='booking-user-list-cancelled-rejected'),
+    path('completed/', BookingUserListCompletedAPIView.as_view(), name='booking-user-list-completed'),
+    path('all/', BookingOwnerListAllAPIView.as_view(), name='booking-user-list-all'),
+
+    path('<uuid:booking_uuid>/', BookingUserUserDetailAPIView.as_view(), name='booking-user-detail'),
+    path('<uuid:booking_uuid>/cancel/', BookingUserUserCancelAPIView.as_view(), name='booking-user-cancel'),
 ]
