f1c3bd7b4134015a560012227e1a63ea1cbfc571
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-21 12:46:21 +0200

    Added documentation.

--- apps/bookings/views/owner_views.py
+++ apps/bookings/views/owner_views.py
@@ -16,38 +16,86 @@ from apps.bookings.serializers import (
     BookingOwnerListSerializer,
 )
 
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiResponse,
+)
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="All bookings for this property",
+    description="The property owner sees all bookings with all statuses.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListAllAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user)
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="All active bookings for this property",
+    description="The property owner sees active bookings.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListActiveAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user).active()
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Confirmed bookings for this property",
+    description="The property owner sees all confirmed bookings.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListConfirmedAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user).confirmed()
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Pending bookings for this property",
+    description="The property owner sees all pending bookings.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListPendingAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user).pending()
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Cancelled/Rejected bookings for this property",
+    description="The property owner sees all cancelled and rejected bookings.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListCancelledOrRejectedAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user).cancelled_rejected()
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Completed bookings for this property",
+    description="The property owner sees all completed bookings.",
+    responses=BookingOwnerListSerializer(many=True)
+)
 class BookingOwnerListCompletedAPIView(ListAPIView):
     serializer_class = BookingOwnerListSerializer
     def get_queryset(self):
         return Booking.objects.with_details().for_owner(self.request.user).completed()
 
-
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Property owner Booking details",
+    description="""
+    Returns detailed information about a booking.
+    Accessible only to the owner of the property associated with the booking.
+    """,
+    responses=BookingOwnerDetailSerializer,
+)
 class BookingOwnerDetailAPIView(RetrieveAPIView, BookingOwnerLookupMixin):
     serializer_class = BookingOwnerDetailSerializer
     lookup_field = 'uuid'
@@ -56,6 +104,24 @@ class BookingOwnerDetailAPIView(RetrieveAPIView, BookingOwnerLookupMixin):
         return Booking.objects.with_details().for_owner(self.request.user)
 
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Confirm booking",
+    description="""
+Confirm an existing booking.
+
+Confirmation is allowed only:
+- for PENDING bookings;
+
+Returns updated booking.
+""",
+    responses={
+        200: BookingOwnerDetailSerializer,
+        400: OpenApiResponse(description='Confirm is not allowed'),
+        403: OpenApiResponse(description='Permission denied'),
+        404: OpenApiResponse(description='Booking not found'),
+    }
+)
 class BookingOwnerConfirmAPIView(APIView, BookingOwnerLookupMixin):
     def patch(self, request, booking_uuid):
         booking = self.get_booking()
@@ -63,7 +129,24 @@ class BookingOwnerConfirmAPIView(APIView, BookingOwnerLookupMixin):
         serializer = BookingOwnerDetailSerializer(booking)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Reject booking",
+    description="""
+Reject an existing booking.
 
+Reject is allowed only:
+- for PENDING bookings;
+
+Returns updated booking.
+""",
+    responses={
+        200: BookingOwnerDetailSerializer,
+        400: OpenApiResponse(description='Reject is not allowed'),
+        403: OpenApiResponse(description='Permission denied'),
+        404: OpenApiResponse(description='Booking not found'),
+    }
+)
 class BookingOwnerRejectAPIView(APIView, BookingOwnerLookupMixin):
     def patch(self, request, booking_uuid):
         booking = self.get_booking()
@@ -72,6 +155,26 @@ class BookingOwnerRejectAPIView(APIView, BookingOwnerLookupMixin):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@extend_schema(
+    tags=['Property owner Bookings'],
+    summary="Reject booking",
+    description="""
+Complete an existing booking.
+
+Completion is allowed only:
+
+- for CONFIRMED bookings;
+- after the check-out date.
+
+Returns updated booking.
+""",
+    responses={
+        200: BookingOwnerDetailSerializer,
+        400: OpenApiResponse(description='Complete is not allowed'),
+        403: OpenApiResponse(description='Permission denied'),
+        404: OpenApiResponse(description='Booking not found'),
+    }
+)
 class BookingOwnerCompleteAPIView(APIView, BookingOwnerLookupMixin):
     def patch(self, request, booking_uuid):
         booking = self.get_booking()
