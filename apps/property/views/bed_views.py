3e091f2484169eed086d44f1a42330a62f3dd3af
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-21 12:43:21 +0200

    Added documentation.

--- apps/property/views/bed_views.py
+++ apps/property/views/bed_views.py
@@ -3,8 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.property.models import Bed
+from apps.property.serializers.bed_serializers import ChoiceSerializer
+
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(
+    tags=['Catalogs'],
+    summary="List bed types",
+    description="Returns all available bed type choices.",
+    responses=ChoiceSerializer(many=True),
+)
 class BedTypeChoicesAPIView(APIView):
     permission_classes = [AllowAny]
 
