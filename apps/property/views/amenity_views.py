b2e67b72a5c82bbb8d844c101bca1f99bcea9396
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-07-21 12:42:37 +0200

    Added documentation.

--- apps/property/views/amenity_views.py
+++ apps/property/views/amenity_views.py
@@ -3,8 +3,15 @@ from rest_framework.permissions import AllowAny
 
 from apps.property.models import Amenity
 from apps.property.serializers import AmenitySerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(
+    tags=['Catalogs'],
+    summary="List amenities",
+    description="Returns a list of all available property or units amenities.",
+    responses=AmenitySerializer(many=True),
+)
 class AmenityListAPIView(ListAPIView):
     permission_classes = [AllowAny]
     serializer_class = AmenitySerializer
